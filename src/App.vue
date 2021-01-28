81a9268638134d01d84703d6359651c0b1665b73
Mikhail Kuznetcov <Mikhail.Kuznetcov@ing.com>
2021-01-28 19:12:10 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
+</template>
+<script>
+export default {
+  name: 'MainApp',
+  created () {
+    console.log('Hello from Main app')
+    console.log('Main app router', this.$route)
+    console.log('Main app store:', this.$store)
+  }
+}
+</script>
+
+<style>
+#main-app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
